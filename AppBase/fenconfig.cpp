ddffd3149a9cf484575826b5ef0e82965a774aed
Marc Plano-Lesay <marc31boss@gmail.com>
2010-03-03 14:36:46 +0100

    Fenêtre de configuration : Ajout des champs largeur/hauteur de la carte, nombre de femelles/mâles/petits, utilisation de ces paramètres pour la création de la carte/monde

--- AppBase/fenconfig.cpp
+++ AppBase/fenconfig.cpp
@@ -6,13 +6,43 @@
 
 #include <QtCore/QCoreApplication>
 #include <QtGui/QDialogButtonBox>
-#include <QtGui/QHBoxLayout>
+#include <QtGui/QFormLayout>
+#include <QtGui/QGroupBox>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QSpinBox>
 
 FenConfig::FenConfig(FenCarte* pFenCarte, Monde* pMonde, QWidget* pParent) : QWidget(pParent), m_fenCarte(pFenCarte), m_monde(pMonde) {
     setAttribute(Qt::WA_DeleteOnClose);
     setWindowTitle(tr("Bel-O-Kan - Initialisation du monde"));
-    QHBoxLayout* layout = new QHBoxLayout;
+    QVBoxLayout* layout = new QVBoxLayout;
+
+    QGroupBox *carteGroupBox = new QGroupBox(tr("Carte"));
+    QFormLayout *carte = new QFormLayout;
+    carteGroupBox->setLayout(carte);
+    m_largeurCarte = new QSpinBox(carteGroupBox);
+    m_largeurCarte->setRange(1, 50);
+    m_hauteurCarte = new QSpinBox(carteGroupBox);
+    m_hauteurCarte->setRange(1, 50);
+    carte->addRow(tr("Largeur"), m_largeurCarte);
+    carte->addRow(tr("Hauteur"), m_hauteurCarte);
+
+    QGroupBox *peupleGroupBox = new QGroupBox(tr("Peuple"));
+    QFormLayout *peuple = new QFormLayout;
+    peupleGroupBox->setLayout(peuple);
+    m_nbFemelles = new QSpinBox(peupleGroupBox);
+    m_nbFemelles->setRange(1, 50);
+    m_nbMales = new QSpinBox(peupleGroupBox);
+    m_nbMales->setRange(1, 50);
+    m_nbPetits = new QSpinBox(peupleGroupBox);
+    m_nbPetits->setRange(1, 50);
+    peuple->addRow(tr("Femelles"), m_nbFemelles);
+    peuple->addRow(tr("Mâles"), m_nbMales);
+    peuple->addRow(tr("Petits"), m_nbPetits);
+
     m_bbox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+    layout->addWidget(carteGroupBox);
+    layout->addWidget(peupleGroupBox);
     layout->addWidget(m_bbox);
     setLayout(layout);
 
@@ -21,16 +51,21 @@ FenConfig::FenConfig(FenCarte* pFenCarte, Monde* pMonde, QWidget* pParent) : QWi
 }
 
 FenConfig::~FenConfig() {
+    delete m_largeurCarte;
+    delete m_hauteurCarte;
+    delete m_nbFemelles;
+    delete m_nbMales;
+    delete m_nbPetits;
     delete m_bbox;
 }
 
 void FenConfig::lancer() {
     ParamsMonde p;
-    p.hauteur = 15;
-    p.largeur = 11;
-    p.nbFemelles = 3;
-    p.nbMales = 3;
-    p.nbPetits = 2;
+    p.hauteur = m_hauteurCarte->value();
+    p.largeur = m_largeurCarte->value();
+    p.nbFemelles = m_nbFemelles->value();
+    p.nbMales = m_nbMales->value();
+    p.nbPetits = m_nbPetits->value();
     m_monde->init(p);
     m_fenCarte->show(p.hauteur, p.largeur);
     close();
--- AppBase/fenconfig.h
+++ AppBase/fenconfig.h
@@ -6,6 +6,7 @@
 class FenCarte;
 class Monde;
 class QDialogButtonBox;
+class QSpinBox;
 
 class FenConfig : public QWidget {
     Q_OBJECT
@@ -21,6 +22,12 @@ private:
     FenCarte* m_fenCarte;
     Monde* m_monde;
     QDialogButtonBox* m_bbox;
+    QSpinBox* m_largeurCarte;
+    QSpinBox* m_hauteurCarte;
+
+    QSpinBox* m_nbFemelles;
+    QSpinBox* m_nbMales;
+    QSpinBox* m_nbPetits;
 };
 
 #endif // FENCONFIG_H
